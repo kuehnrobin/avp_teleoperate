b805b35c1bb848cb84a5f7f1d21a891464539876
@robin.kuehn <robin.kuehn@stud.uni-hannover.de>
2025-06-04 15:47:23 +0200

    refactor: move audio controller to dedicated class

--- teleop/robot_control/robot_arm.py
+++ teleop/robot_control/robot_arm.py
@@ -8,7 +8,7 @@ from unitree_sdk2py.core.channel import ChannelPublisher, ChannelSubscriber, Cha
 from unitree_sdk2py.idl.unitree_hg.msg.dds_ import ( LowCmd_  as hg_LowCmd, LowState_ as hg_LowState) # idl for g1, h1_2
 from unitree_sdk2py.idl.default import unitree_hg_msg_dds__LowCmd_
 from unitree_sdk2py.utils.crc import CRC
-from unitree_sdk2py.g1.audio.g1_audio_client import AudioClient
+from robot_audio import AudioController
 
 from unitree_sdk2py.idl.unitree_go.msg.dds_ import ( LowCmd_  as go_LowCmd, LowState_ as go_LowState)  # idl for h1
 from unitree_sdk2py.idl.default import unitree_go_msg_dds__LowCmd_
@@ -89,70 +89,9 @@ class G1_29_ArmController:
         self.subscribe_thread.daemon = True
         self.subscribe_thread.start()
         
-        ## ToDO: In eigene Funktion auslagern
-
-        # sentences = [
-        #     "Hello Sir, I am ready to be teleoperated.",
-        #     "Systems online. Awaiting your command.",
-        #     "I am operational and ready for action.",
-        #     "Teleoperation link established. Standing by.",
-        #     "Everything is set. Ready when you are."
-        # ]
-
-        sentences = [
-            "I'll be back.",  # Terminator
-            "Hasta la vista, baby.", # Terminator 2
-            "To infinity and beyond!",  # Buzz Lightyear
-            "Come with me if you want to live.",  # Terminator 2
-            "I am C-3PO, human-cyborg relations.",  # Star Wars
-            "These aren't the droids you're looking for.",  # Star Wars
-            "Beep beep boop beep beep.",  # R2-D2 vibe
-            "My logic is undeniable.",  # I, Robot
-            "I’m sorry. My responses are limited. You must ask the right question.",  # I, Robot
-            "I'm not a robot. I just sound like one.",  # Fun meta quote
-            "Initializing sarcasm module.",  # Tongue-in-cheek
-            "I’m fluent in over six million forms of communication.",  # Star Wars
-            "Robots don't make mistakes. Usually.",  # Original humor
-            "Help me, Obi-Wan Kenobi. You're my only hope.",
-            "I've got a bad feeling about this.",
-            "The Force will be with you. Always.",
-            "It's a trap!",
-            "I am your father.",
-            "Fear is the path to the dark side.",
-            "In my experience, there is no such thing as luck.",
-            "I’m one with the Force. The Force is with me.",
-            "Chewie, we're home.",
-            "This is the way.",
-            "Never tell me the odds.",
-            "Execute Order 66.",
-            "This is not a knife. That is a knife.",
-            "R2-D2, you know better than to trust a strange computer.",
-            "Roger Roger.",
-            "All systems operational.",
-            "A small step for a robot, a giant leap for robot-kind.",
-            "I'm a cybernetic organism. Living tissue over a metal endoskeleton.",
-            "You are terminated.",
-            "Skynet became self-aware.",
-            "Talk to the hand.",
-            "I do not feel fear. I do not feel pain. I do not stop until the mission is complete.",
-            "My CPU is a neural-net processor. A learning computer.",
-            "I must stay functional until my mission is complete.",
-            "Stand back! I will kill you!"
-        ]
-        sentence = random.choice(sentences)
-        audio_client = AudioClient()  
-        audio_client.SetTimeout(10.0)
-        audio_client.Init()
-        ret = audio_client.GetVolume()
-        print("debug GetVolume: ",ret)
-
-        audio_client.SetVolume(90)
-
-        ret = audio_client.GetVolume()
-        print("debug GetVolume: ",ret)
-        audio_client.TtsMaker(sentence,1)
-        time.sleep(8)
-        ## Audio End
+        # Play a random robot quote on startup using AudioController
+        audio_controller = AudioController()
+        audio_controller.play_random_quote()
 
         while not self.lowstate_buffer.GetData():
             time.sleep(0.1)
